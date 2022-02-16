79ffff2e89588567c08cdbc014ef1a082ad13aa4
Murat Halici <mhalici3@gmail.com>
2022-02-16 10:10:20 -0500

    code refactoring

--- script.js
+++ script.js
@@ -26,10 +26,11 @@ let weather = {
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°F";
+    document.querySelector(".temp").innerText = Math.round(temp) + "°F";
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + Math.round(speed) + " mph";
     document.querySelector(".weather").classList.remove("loading");
     weather.getBackgroundImage(localStorage.getItem("city"));
     document.querySelector(".search-bar").value = name;
@@ -40,19 +41,21 @@ let weather = {
     this.fetchWeather(city);
   },
   getBackgroundImage: function (city) {
-    let backgroundImage = fetch(
-      `https://api.unsplash.com/search/photos?query=${city}&page=1&per_page=30&client_id=your-key-goes-here`
+    console.log("running getBackgroundImage");
+    fetch(
+      `https://api.unsplash.com/search/photos?query=${city}&page=1&per_page=30&client_id=FRV-YYIQmUcBLvg44HVgWvzfZXetydbVMgOizbxzxDo`
     )
       .then((response) => {
         return response.json();
       })
       .then((data) => {
+        let backgroundImages = [];
         data.results.forEach((element, key) => {
-          // Pick a random image from the array
-          imageKey = Math.floor(Math.random() * data.results.length);
-          let image = data.results[imageKey].urls.regular;
-          document.body.style.backgroundImage = `url(${image})`;
+          backgroundImages.push(data.results[key].urls.regular);
         });
+        imageKey = Math.floor(Math.random() * backgroundImages.length);
+        let backgroundImage = backgroundImages[imageKey];
+        document.body.style.backgroundImage = `url(${backgroundImage})`;
       });
   },
 };
